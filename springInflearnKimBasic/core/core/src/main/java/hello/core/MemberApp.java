6193babeff0adc0eda97cc3cc91381644eae5d14
kangmoonsun <kangmoonsun9@naver.com>
2023-04-23 21:13:03 +0900

    인텔리제이 깃 연동확인용

--- springInflearnKimBasic/core/core/src/main/java/hello/core/MemberApp.java
+++ springInflearnKimBasic/core/core/src/main/java/hello/core/MemberApp.java
@@ -13,6 +13,7 @@ public class MemberApp {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
 
+
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
@@ -22,7 +23,5 @@ public class MemberApp {
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());
-        //
-
     }
 }
