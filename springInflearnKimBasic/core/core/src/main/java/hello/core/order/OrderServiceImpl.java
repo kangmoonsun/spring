6afd586b63ef8b390711d3f2a324230cd392f2d8
kangmoonsun <kangmoonsun9@naver.com>
2023-04-23 20:37:27 +0900

    스프링핵심원리 기본편"

---
+++ springInflearnKimBasic/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy ;
+    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); 위반의 흔적
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order CreateOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+        // SRP - 단일 체계 원칙이 아주 잘됐다
+    }
+}
