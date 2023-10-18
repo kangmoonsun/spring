1b9baefa105b95c0c3eb369cc2f0599adcc10775
kangmoonsun <kangmoonsun9@naver.com>
2023-10-18 20:11:52 +0900

    스프링 공부중

--- jdbc/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -4,14 +4,14 @@ import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
  */
 
+// low하게 짜는거야
 
 @Slf4j
 public class MemberRepositoryV0 {
@@ -27,15 +27,117 @@ public class MemberRepositoryV0 {
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, member.getMemberId());
             pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
             return member;
         } catch (SQLException e) {
             log.error("db error",e);
             throw e; // 밖으로 던짐
         } finally {
-            pstmt.close();
-            con.close();
-            // 안닫아주면 계속 돌아다님
+            close(con, pstmt, null);
+
         }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_Id =?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()){
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else{
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize={}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error",e);
+            throw e; // 밖으로 던짐
+        } finally {
+            close(con, pstmt, null);
+
+        }
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error("db error",e);
+            throw e; // 밖으로 던짐
+        } finally {
+            close(con, pstmt, null);
+
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs){
+
+        if(rs != null){
+            try {
+                rs.close(); // SQLException
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if(stmt != null){
+            try {
+                stmt.close(); // SQLException
+            } catch (SQLException e) {
+                log.info("error", e);
+                // 에러 터져도 잡기 때문에 아래 con.close문이 실행된다
+            }
+        }
+
+        if(con != null){
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+        // 안닫아주면 계속 돌아다님
+        // 시작과 역순으로 닫아준다
     }
 
     private Connection getConnection() {
